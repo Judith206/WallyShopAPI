f923e7892ffaf264c998c8b344e86b39dce2228a
cp25001@esfe.agape.edu.sv <cp25001@esfe.agape.edu.sv>
2025-09-10 14:00:49 -0600

    controladores

---
+++ WallyShopAPI/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WallyShopAPI.DTOs.UsuarioDTOs;
+using WallyShopAPI.Interfaces;
+
+namespace WallyShopAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("registrar")]
+        public async Task<IActionResult> Registrar([FromBody] UsuarioRegistroDTO dto)
+        {
+            var result = await _authService.RegistrarAsync(dto);
+            return Ok(result);
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] UsuarioLoginDto dto)
+        {
+            var result = await _authService.LoginAsync(dto);
+            if (result == null) return Unauthorized("Credenciales invalidas");
+            return Ok(result);
+        }
+    }
+}
---
+++ WallyShopAPI/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WallyShopAPI.Interfaces;
+
+namespace WallyShopAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+        public UserController(IUsuarioRepository pUsuarioRepository)
+        {
+            _usuarioRepository = pUsuarioRepository;
+        }
+        [HttpGet("usuarios")]
+        [Authorize]
+        public async Task<IActionResult> GetUsuarios()
+        {
+            var usuarios = await _usuarioRepository.GetAllUsuariosAsync();
+            return Ok(usuarios);
+        }
+    }
+}
